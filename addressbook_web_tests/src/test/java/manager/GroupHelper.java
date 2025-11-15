46d8b35dcd6c48bd98a372089eacb87a7f911a1e
anna.rzheutskaya <anna.rzheutskaya@zimad.com>
2025-11-16 01:38:03 +0300

    Задание № 6

---
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -0,0 +1,39 @@
+package manager;
+
+import model.GroupData;
+import org.openqa.selenium.By;
+
+public class GroupHelper extends HelperBase {
+
+    public GroupHelper(ApplicationManager manager){
+        super(manager);
+    }
+
+    public void openGroupsPage() {
+        if (!manager.isElementPresent(By.name("new"))) {
+            click(By.linkText("groups"));
+        }
+    }
+
+    public boolean isGroupPresent() {
+        openGroupsPage();
+        return manager.isElementPresent(By.name("selected[]"));
+    }
+
+    public void createGroup(GroupData group) {
+        openGroupsPage();
+        click(By.name("new"));
+        type(By.name("group_name"),group.name());
+        type(By.name("group_header"),group.header());
+        type(By.name("group_footer"),group.footer());
+        click(By.name("submit"));
+        click(By.linkText("group page"));
+    }
+
+    public void removeGroup() {
+        openGroupsPage();
+        click(By.name("selected[]"));
+        click(By.name("delete"));
+        click(By.linkText("group page"));
+    }
+}
---
+++ addressbook_web_tests/src/test/java/tests/RemoveGroupTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class RemoveGroupTest extends TestBase{
+
+    @Test
+    public void CanDeleteGroup() {
+        if (!app.groups().isGroupPresent()){
+            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+        }
+        app.groups().removeGroup();
+
+    }
+
+
+}
